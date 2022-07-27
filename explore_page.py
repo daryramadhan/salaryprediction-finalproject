a8fb54912508aaf1eb28e184c0a1bd86ae6d15ac
Dary Ramadhan Fajar <72384479+daryramadhan@users.noreply.github.com>
2022-07-27 20:09:49 +0700

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -32,7 +32,7 @@ def clean_education(x):
 
 @st.cache   
 def load_data():
-    df = "D:/Kampus/Semester 4/Machine Learning/Final Project/Survey Data/survey_results.csv"
+    df = "survey_results.csv"
     df = pd.read_csv(df)
     df = df[["Province", "EdLevel", "YearsCodePro", "Employment", "ConvertedCompYearly"]]
     df = df[df["ConvertedCompYearly"].notnull()]
